f48f3456826b5a51d4bd1584789cda1b635c1e82
Ilya Sukharev LapTop <imsworkfor@gmail.com>
2023-03-22 14:26:20 +0300

    starting toDo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import '../App.css';
 // import Users from './components/users';
 
 function App() {
-  
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
   return (
     <div className="App">
+      <label>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </label>
     </div>
   );
 }
